bf1ab241e755ed000cd0a53c27fe390c598ac606
Isayd Gallegos Flores <isayd@isayd.local>
2017-03-08 13:42:01 -0800

    Rename Example Page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -13,7 +13,7 @@ var React = require('react');
 var Examples = (props) =>{
   return (
       <div>
-        <h1>Examples Component!!!</h1>
+        <h1>Examples</h1>
         <p>Welcome to Examples Page!</p>
       </div>
   )
